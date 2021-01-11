b8ed4d4c6e2679caeda55eb5819974d9e91d9345
YanaLugina <yana4961@gmail.com>
2021-01-12 02:18:10 +1000

    addMicrofrontend: added microfrontend in app container. Yippee!

---
+++ src/components/App/App.js
@@ -0,0 +1,27 @@
+import TodoApp from '../TooApp';
+import PropTypes from 'prop-types';
+
+const App = ({ todoData:data, handleMouseLeave }) => {
+    const initialHandleMouseLeave = (items) => {
+        console.log('initialHandleMouseLeave', items)
+    };
+
+    const todoData = [
+        { label: 'one', id: 1, done: false, important: true },
+        { label: 'two', id: 2, done: false, important: false },
+        { label: 'three', id: 3, done: true, important: true },
+    ];
+
+    return (
+        <>
+            <TodoApp todoData={todoData || data} handleMouseLeave={handleMouseLeave || initialHandleMouseLeave} />
+        </>
+    );
+};
+
+App.propTypes = {
+    todoData: PropTypes.array,
+    handleMouseLeave: PropTypes.func
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// render micro frontend function
+window.rendercreatereactapp = (containerId, history) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App history={history}/>
+        </React.StrictMode>,
+        document.getElementById(containerId)
+    );
+};
+
+// unmount micro frontend function
+window.unmountcreatereactapp = containerId => {
+    ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+// Mount to root if it is not a micro frontend
+if (!document.getElementById('createreactapp-container')) {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
